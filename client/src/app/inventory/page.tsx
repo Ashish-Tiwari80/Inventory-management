f7f11da5707236fe9ce4202161cc5ae7c63657f5
Ashish Tiwari <ashishtiwa970@gmail.com>
2026-03-22 19:47:41 +0530

    Fixed client folder & added all files

---
+++ client/src/app/inventory/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useGetProductsQuery } from "../state/api";
+
+const Inventory = () => {
+  const { data: products, isError, isLoading } = useGetProductsQuery();
+
+  return <div>Inventory</div>;
+};
+
+export default Inventory;
